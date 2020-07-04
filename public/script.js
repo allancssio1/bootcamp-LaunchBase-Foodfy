64b92476ae9d322d9f388bb94866877d5a88f032
Allan Freitas <allan.cassio1@gmail.com>
2020-07-04 11:48:48 -0300

    concluido o desafio 3 do foodfy

--- public/script.js
+++ public/script.js
@@ -1,4 +1,11 @@
 const cards = document.querySelectorAll('.card')
+const buttomIngredient = document.querySelector('.buttom-ingredient')
+const buttomPreparation = document.querySelector('.buttom-preparation')
+const buttomInformation = document.querySelector('.buttom-information')
+const ingredients = document.querySelectorAll('.content-ingredient')
+const preparations = document.querySelectorAll('.content-preparation')
+const informations = document.querySelectorAll('.content-information')
+
 
 for(let card of cards){
     card.addEventListener('click', function(){
@@ -6,3 +13,56 @@ for(let card of cards){
         window.location.href = `/recipe/${idCard}`
     } )
 }
+
+function mostrar (element) {
+    if (element == 'Esconder') {
+      return true
+    }
+}
+function esconder (element) {
+    if (element == 'Mostrar') {
+        return true
+    }
+}
+
+buttomIngredient.addEventListener('click', function () {
+    if (mostrar (buttomIngredient.innerHTML) == true) {
+        buttomIngredient.innerHTML = 'Mostrar'
+        for (const ingredient of ingredients) {
+            ingredient.classList.add('no-display')
+        }
+    }else if( esconder (buttomIngredient.innerHTML) == true){
+        buttomIngredient.innerHTML = 'Esconder'
+        for (const ingredient of ingredients) {
+            ingredient.classList.remove('no-display')
+        }
+    }
+})
+
+buttomPreparation.addEventListener('click', function () {
+    if (mostrar (buttomPreparation.innerHTML) == true) {
+        buttomPreparation.innerHTML = 'Mostrar'
+        for (const preparation of preparations) {
+            preparation.classList.add('no-display')
+        }
+    }else if( esconder (buttomPreparation.innerHTML) == true){
+        buttomPreparation.innerHTML = 'Esconder'
+        for (const preparation of preparations) {
+            preparation.classList.remove('no-display')
+        }
+    }
+})
+
+buttomInformation.addEventListener('click', function () {
+    if (mostrar (buttomInformation.innerHTML) == true) {
+        buttomInformation.innerHTML = 'Mostrar'
+        for (const information of informations) {
+            information.classList.add('no-display')
+        }
+    }else if( esconder (buttomInformation.innerHTML) == true){
+        buttomInformation.innerHTML = 'Esconder'
+        for (const information of informations) {
+            information.classList.remove('no-display')
+        }
+    }
+})
